0d333ff5263eab203b6cda15f91bcb3f2b3ced74
erickyb <richardyb1985@gmail.com>
2023-09-30 17:53:28 -0500

    archivo 1 terminado y funcionando

--- 1.http.js
+++ 1.http.js
@@ -6,6 +6,27 @@ const desiredPort = process.env.PORT ?? 1234
 const processRequest = (req, res) => {
   res.setHeader('Content-Type', 'text/html;charset=utf-8')
   if (req.url === '/') {
-
+    res.end('<h1>Mi página</h1>')
+  } else if (req.url === '/imagen-super-bonita.png') {
+    fs.readFile('./placa.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internal Server Error</h1>')
+      } else {
+        res.setHeader('content-Type', 'image/png')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contacto') {
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404
+    res.end('<h1>404</h1>')
   }
 }
+
+const server = http.createServer(processRequest)
+
+server.listen(desiredPort, () => {
+  console.log(`server listening on port http://localhost:${desiredPort}`)
+})
